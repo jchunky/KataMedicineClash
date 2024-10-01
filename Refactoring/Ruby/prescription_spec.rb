d8dbcf487171eb88c13bb901ae1250f0073a2c8b
Jason CheongKeeYou <jasonc@nulogy.com>
2024-09-30 22:38:48 -0400

    Update spec syntax

--- Refactoring/Ruby/prescription_spec.rb
+++ Refactoring/Ruby/prescription_spec.rb
@@ -9,7 +9,7 @@ describe Prescription do
       Prescription.new(:dispense_date => 15.days.ago, :days_supply => 30)
     end
     it 'is <days_supply> after <dispense_date>' do
-      prescription.completion_date.should == 15.days.from_now.to_date
+      expect(prescription.completion_date).to eq(15.days.from_now.to_date)
     end
   end
 end
