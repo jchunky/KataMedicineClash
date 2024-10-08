b5b089e7df2394948a1dadf8c98c32bc84401c00
Jason CheongKeeYou <jasonc@nulogy.com>
2024-10-07 20:49:41 -0400

    refactor

--- Refactoring/Ruby/medicine.rb
+++ Refactoring/Ruby/medicine.rb
@@ -1,30 +1,33 @@
 class Medicine
-  
   attr_reader :name, :prescriptions
-  
+
   def initialize(name)
     @name = name
     @prescriptions = []
   end
-  
-  def first_prescription
-    @first_prescription ||= prescriptions.sort.first
+
+  def dates_prescribed_in_effective_range(day_count)
+    dates_prescribed(day_count).select do |d|
+      possession_effective_range(day_count).include?(d)
+    end
   end
 
+  private
+
   def last_prescription
-    @last_prescription ||= prescriptions.sort.last
+    @last_prescription ||= prescriptions.max
   end
-  
+
   def days_supply
     valid_prescriptions? ? last_prescription.days_supply : read_attribute(:days_supply)
   end
-  
+
   def possession_end_date
-    most_recent_prescription.completion_date 
+    most_recent_prescription.completion_date
   end
 
   def possession_effective_end_date
-    [possession_end_date, Date.today].min
+    Date.today
   end
 
   def possession_ratio_lower_bound_date(day_count)
@@ -36,15 +39,11 @@ class Medicine
   end
 
   def initial_dispense_date
-    first_prescription.dispense_date
+    prescriptions.min.dispense_date
   end
 
   def most_recent_prescription
-    prescriptions.sort_by(&:dispense_date).last
-  end
-
-  def number_of_days_prescribed(day_count)
-    dates_prescribed_in_effective_range(day_count).size
+    prescriptions.max_by(&:dispense_date)
   end
 
   def number_of_days_in_range(day_count)
@@ -61,20 +60,11 @@ class Medicine
 
   def dates_prescribed(day_count)
     prescriptions_in_range(day_count).map do |p|
-      (p.dispense_date...p.dispense_date.advance(:days => p.days_supply)).to_a
+      (p.dispense_date...p.dispense_date.advance(days: p.days_supply)).to_a
     end.flatten.uniq
   end
 
-  def dates_prescribed_in_effective_range(day_count)
-    dates_prescribed(day_count).select do |d|
-      possession_effective_range(day_count).include?(d)
-    end
-  end
-
-  protected
-  
   def possession_effective_range(day_count)
     possession_effective_start_date(day_count)...possession_effective_end_date
   end
-
 end
--- Refactoring/Ruby/prescription.rb
+++ Refactoring/Ruby/prescription.rb
@@ -1,23 +1,22 @@
 class Prescription
+  include Comparable
 
   attr_reader :dispense_date, :days_supply
 
-  def initialize(options = {})
-    @dispense_date = options[:dispense_date] ||= Date.today
-    @days_supply = options[:days_supply] ||= 30
+  def initialize(dispense_date:, days_supply:)
+    @dispense_date = dispense_date
+    @days_supply = days_supply
   end
 
   def <=>(other)
-    return -1 if dispense_date.nil?
-    return 1 if other.dispense_date.nil?
     dispense_date <=> other.dispense_date
+  end
+
+  def days_taken
+    (dispense_date...completion_date)
   end
 
   def completion_date
     dispense_date + days_supply
-  end
-
-  def days_taken
-    (dispense_date...(dispense_date + days_supply)).to_a
   end
 end
