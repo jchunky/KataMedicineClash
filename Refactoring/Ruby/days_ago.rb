de19ee2d494209d66781c3fdb0b08440109ad10f
Jason CheongKeeYou <jasonc@nulogy.com>
2024-10-20 21:24:07 -0400

    refactor

--- Refactoring/Ruby/days_ago.rb
+++ Refactoring/Ruby/days_ago.rb
@@ -1,8 +1,8 @@
 require "date"
 
 class Integer
-  def day      = self
-  def days     = self
-  def ago      = Date.today - self
+  def day = self
+  def days = self
+  def ago = Date.today - self
   def from_now = Date.today + self
 end
--- Refactoring/Ruby/prescription.rb
+++ Refactoring/Ruby/prescription.rb
@@ -1,15 +1,4 @@
-class Prescription
-  attr_reader :dispense_date, :days_supply
-
-  def initialize(dispense_date:, days_supply:)
-    @dispense_date = dispense_date
-    @days_supply = days_supply
-  end
-
-  def <=>(other)
-    dispense_date <=> other.dispense_date
-  end
-
-  def days_taken      = (dispense_date...completion_date).to_a
+Prescription = Data.define(:dispense_date, :days_supply) do
+  def days_taken = (dispense_date...completion_date).to_a
   def completion_date = dispense_date + days_supply
 end
